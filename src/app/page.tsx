130594c74766cace984c532b6bd8e2ac3c8e5dc4
Jerry <jiahuagoooo@gmail.com>
2025-01-27 21:37:11 +0800

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,16 @@
-<<<<<<< HEAD
 'use client';
 
 import { Header } from '@/components/Header';
 import { Suspense } from 'react';
-=======
-
-import { Header } from '@/components/Header';
-import { CreateTokenButton } from '@/components/CreateTokenButton';
-import { PhantomTransactionButton } from '@/components/PhantomTransactionButton';
-
->>>>>>> main
 
 export default function Home() {
   return (
     <main className="min-h-screen relative">
       <div className="relative z-10">
         <Header />
-<<<<<<< HEAD
         <Suspense fallback={<div>Loading...</div>}>
         1
         </Suspense>
-=======
-        <PhantomTransactionButton 
-          senderAddress="FriELggez2Dy3phZeHHAdpcoEXkKQVkv6tx3zDtCVP8T" 
-          recipientAddress="3FSqX4cqGqkc3GQrxz8iRWGNtUsiFPKJF7FaxMy8fpdP" 
-          amount={100000000} 
-        />
->>>>>>> main
       </div>
     </main>
   );
